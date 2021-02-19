8d3a22c855825cb025b42f6148d534a08c20906f
Theranmeadows97 <theranmeadows@gmail.com>
2021-02-18 19:09:56 -0700

    commit

--- lesson7/script.js
+++ lesson7/script.js
@@ -1,24 +1,32 @@
-const images = document.querySelectorAll("[data-src]");
+const imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const loadImages = (image) => {
-    image.setAttribute("src", image.getAttribute("data-src"));
-    image.onload = () => {image.removeAttribute("data-src");
-    };
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
 };
 
-if("IntesectionObserver" in window){
-    const imgObserver = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-        });
-    }, imgOptions);
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 200px 0px"
+};
 
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    }, imgOptions);
     imagesToLoad.forEach((img) => {
-        imgObserver.observe(img);
-    }); 
-}
-else {
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
+      observer.observe(img);
     });
-}
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
 
