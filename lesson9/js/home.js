c580d5459350256efde82a61fd2077d7ef6430e1
Theranmeadows97 <theranmeadows@gmail.com>
2021-03-08 03:54:17 -0700

    commit

---
+++ lesson9/js/home.js
@@ -0,0 +1,46 @@
+
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+.then(function (response) {
+   return response.json();
+})
+.then(function (jsonObject) {
+   //console.table(jsonObject);  // temporary checking for valid response and data parsing
+   const towns = jsonObject['towns'];
+   towns.sort((a,b) => (a.averageRainfall > b.averageRainfall) ? 1 : ((b.averageRainfall > a.averageRainfall) ? -1 : 0));
+   towns.reverse();
+
+   for (let i = 0; i < towns.length; i++) {
+      if (towns[i].name == "Preston" || towns[i].name == "Fish Haven" || towns[i].name == "Soda Springs") {
+          
+      let card = document.createElement('section');
+      let name = document.createElement('h2');
+      let motto = document.createElement('h4');
+      let image = document.createElement('img');
+      let yearFounded = document.createElement('p');
+      let currentPopulation = document.createElement('p');
+      let averageRainfall = document.createElement('p');
+      let information = document.createElement('div');
+
+      name.textContent = towns[i].name;
+      motto.textContent = towns[i].motto;
+      currentPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
+      averageRainfall.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall + ' ' + 'inches';
+      yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+      image.setAttribute('src', '/lesson9/images/' + towns[i].photo);
+      image.setAttribute('alt', towns[i].name);
+      information.setAttribute('id', 'information');
+
+      card.appendChild(information);
+      information.appendChild(image);
+      information.appendChild(name);
+      information.appendChild(motto);
+      information.appendChild(currentPopulation);
+      information.appendChild(averageRainfall);
+      information.appendChild(yearFounded);
+      
+
+      document.querySelector('div.cards').appendChild(card);
+      }
+   }
+});
