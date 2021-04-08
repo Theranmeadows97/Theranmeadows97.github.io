9e8d412ede101a91bea807b9dc55c99b59fdb155
Theranmeadows97 <theranmeadows@gmail.com>
2021-04-07 17:05:15 -0700

    commit

---
+++ Chamber of Commerce/directory.js
@@ -0,0 +1,36 @@
+const requestURL = '/Users/theranmeadows/Desktop/BYU-Idaho/Winter 2021/WDD 231/theranmeadows97.github.io/Chamber of Commerce/directory.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['directory'];
+
+    for (let i = 0; i < directory.length; i++ ) {
+      let card = document.createElement('section');
+      let name = document.createElement('h2');
+      let address = document.createElement('p');
+      let phone = document.createElement('p');
+      let email = document.createElement('p');
+      let logo = document.createElement('img');
+      let website = document.createElement('p');
+
+      name.textContent = directory[i].name + (i+1);
+      address.innerHTML = "address: " + directory[i].address;
+      phone.innerHTML = "phone: " + directory[i].phone;
+      email.innerHTML = "email: " + directory[i].email;
+      website.innerHTML = "website: " + directory[i].website;
+      logo.setAttribute('src', directory[i].logo);
+
+      card.appendChild(name);
+      card.appendChild(address);
+      card.appendChild(phone);
+      card.appendChild(email);
+      card.appendChild(website);
+      card.appendChild(logo);
+
+      document.querySelector('div.cards').appendChild(card);
+    }
+  });
